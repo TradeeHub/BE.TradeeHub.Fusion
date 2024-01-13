e7f73a3423773e4ada315fa5d4f1652b5dbab6fb
Marenglen Doci <tradeehub@gmail.com>
2024-01-13 17:13:24 +0000

    - Fusion now working with user service next is to make customer service work as well

--- BE.TradeeHub.Fusion/Program.cs
+++ BE.TradeeHub.Fusion/Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient("Fusion");
+
+builder.Services
+    .AddFusionGatewayServer()
+    .ConfigureFromFile("./gateway.fgp");
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGraphQL();
 
-app.Run();
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/graphql/", permanent: false);
+    }
+    else
+    {
+        await next();
+    }
+});
+
+app.RunWithGraphQLCommands(args);
--- BE.TradeeHub.Fusion/README.txt
+++ BE.TradeeHub.Fusion/README.txt
@@ -9,7 +9,4 @@ dotnet new install HotChocolate.Templates
 dotnet fusion subgraph config set http --url http://localhost:5269 -w BE.TradeeHub.CustomerService.Application
 dotnet fusion subgraph config set http --url http://localhost:5225 -w BE.TradeeHub.UserService
 
-dotnet run --project BE.TradeeHub.CustomerService.Application -- schema export --output schema.graphql
-dotnet run --project BE.TradeeHub.UserService -- schema export --output schema.graphql
-
 dotnet new graphql-gateway -n Gateway
